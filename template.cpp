d0c0d61baebf08222939cbc385f1e505aba69651
Maxi Redigonda <maxiredigonda@gmail.com>
2017-07-20 20:52:01 -0300

    Actualiza la template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define forr(i, a, b) for(int i = (a); i < (b); i++)
+#define forr(i, a, b) for(int i = (a); i < (int) (b); i++)
 #define forn(i, n) forr(i, 0, n)
 #define forall(it, v) for(auto it = v.begin(); it != v.end(); ++it)
 #define dforn(i, n) for(int i = n - 1; i >= 0; i--)
