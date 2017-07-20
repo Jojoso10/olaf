fbae7a140bfd1c3b6637fd77e3a53ed199f11b8d
Maxi Redigonda <maxiredigonda@gmail.com>
2017-07-19 22:54:46 -0300

    Modifica la template

--- template.cpp
+++ template.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 #define forr(i, a, b) for(int i = (a); i < (b); i++)
 #define forn(i, n) forr(i, 0, n)
+#define forall(it, v) for(auto it = v.begin(); it != v.end(); ++it)
 #define dforn(i, n) for(int i = n - 1; i >= 0; i--)
 #define db(v) cerr << #v << " = " << v << endl
+#define pb push_back
 typedef long long ll;
-const int MAXN=;
+const int MAXN = -1;
 
 int main() {
     
