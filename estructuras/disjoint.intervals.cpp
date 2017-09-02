4f6818b07e5b9cce1634cc527cf765614ae12193
Maxi Redigonda <maxiredigonda@gmail.com>
2017-09-01 23:16:05 -0300

    Refactoriza disjoint.intervals.cpp

--- estructuras/disjoint.intervals.cpp
+++ estructuras/disjoint.intervals.cpp
@@ -1,16 +1,18 @@
-bool operator< (const ii &a, const ii &b) {return a.fst<b.fst;}
-//Stores intervals as [first, second]
-//in case of a collision it joins them in a single interval
+bool operator <(const ii &a, const ii &b){ return a.first < b.first; }
+// Guarda intervalos como [first, second]
+// En caso de colision, los une en un solo intervalo
 struct disjoint_intervals {
 	set<ii> segs;
-	void insert(ii v) {//O(lgn)
-		if(v.snd-v.fst==0.) return;//OJO
-		set<ii>::iterator it,at;
+	void insert(ii v){ // O(lg n)
+		if(v.snd - v.fst == 0.0) return; // Cuidado!
+		set<ii>::iterator it, at;
 		at = it = segs.lower_bound(v);
-		if (at!=segs.begin() && (--at)->snd >= v.fst)
-			v.fst = at->fst, --it;
+		if(at != segs.begin() && (--at)->snd >= v.fst){
+			v.fst = at->fst;
+      --it;
+    }
 		for(; it!=segs.end() && it->fst <= v.snd; segs.erase(it++))
-			v.snd=max(v.snd, it->snd);
+			v.snd = max(v.snd, it->snd);
 		segs.insert(v);
 	}
 };
