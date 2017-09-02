fc6d746c91411165602b1a809bf3872f8a538321
Maxi Redigonda <maxiredigonda@gmail.com>
2017-09-01 23:45:27 -0300

    Refactoriza bigint (ufff) y tambien hash un toque

--- string/hash.cpp
+++ string/hash.cpp
@@ -13,16 +13,16 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 
-//Compilar: g++ --std=c++11
-struct Hash{
-	size_t operator()(const ii &a)const{
-		size_t s=hash<int>()(a.fst);
-		return hash<int>()(a.snd)+0x9e3779b9+(s<<6)+(s>>2);
+// Compilar con: g++ --std=c++11
+struct Hash {
+	size_t operator()(const ii &a) const {
+		size_t s = hash<int>()(a.fst);
+		return hash<int>()(a.snd) + 0x9e3779b9 + (s << 6) + (s >> 2);
 	}
-	size_t operator()(const vector<int> &v)const{
-		size_t s=0;
+	size_t operator()(const vector<int> &v) const {
+		size_t s = 0;
 		for(auto &e : v)
-			s ^= hash<int>()(e)+0x9e3779b9+(s<<6)+(s>>2);
+			s ^= hash<int>()(e) + 0x9e3779b9 + (s << 6) + (s >> 2);
 		return s;
 	}
 };
@@ -30,12 +30,12 @@ unordered_set<ii, Hash> s;
 unordered_map<ii, int, Hash> m;//map<key, value, hasher>
 
 int main() {
-    srand(time(NULL));
+  srand(time(NULL));
 	set<size_t> coli;
-    forn(i, 1000000){
-		s.insert(ii(rand()%10000, rand()%10000));
-		coli.insert(Hash()(ii(rand()%10000, rand()%10000)));
-    }
-    dprint(sz(coli));
+  forn(i, 1000000){
+    s.insert(ii(rand()%10000, rand()%10000));
+    coli.insert(Hash()(ii(rand()%10000, rand()%10000)));
+  }
+  dprint(sz(coli));
 	return 0;
 }
