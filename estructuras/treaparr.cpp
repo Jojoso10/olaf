4d06b1c7bd278e847868f71a653879ac94951024
Maxi Redigonda <maxiredigonda@gmail.com>
2017-09-18 11:02:45 -0300

    Refactoriza treaparr

--- estructuras/treaparr.cpp
+++ estructuras/treaparr.cpp
@@ -13,77 +13,78 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 
-typedef ii Value;//ii(profundidad, nodo)
+typedef ii Value; // ii(profundidad, nodo)
 typedef struct node *pnode;
-struct node{
+struct node {
     Value val, mini;
     int dirty;
     int prior, size;
-    pnode l,r,parent;
-    node(Value val): val(val), mini(val), dirty(0), prior(rand()), size(1), l(0), r(0), parent(0) {}
+    pnode l, r, parent;
+    node(Value val):val(val), mini(val), dirty(0), prior(rand()), size(1), l(0), r(0), parent(0) {}
 };
-static int size(pnode p) { return p ? p->size : 0; } 
-void push(pnode p) {//propagar dirty a los hijos(aca para lazy)
-	p->val.fst+=p->dirty;
-	p->mini.fst+=p->dirty;
-	if(p->l) p->l->dirty+=p->dirty;
-	if(p->r) p->r->dirty+=p->dirty;
-	p->dirty=0;
+static int size(pnode p){ return p ? p->size : 0; } 
+void push(pnode p){ // propagar dirty a los hijos (aca para lazy)
+	p->val.fst += p->dirty;
+	p->mini.fst += p->dirty;
+	if(p->l) p->l->dirty += p->dirty;
+	if(p->r) p->r->dirty += p->dirty;
+	p->dirty = 0;
 }
-static Value mini(pnode p) { return p ? push(p), p->mini : ii(1e9, -1); } 
+static Value mini(pnode p){ return p ? push(p), p->mini : ii(1e9, -1); } 
 // Update function and size from children's Value
-void pull(pnode p) {//recalcular valor del nodo aca (para rmq)
+void pull(pnode p){ // recalcular valor del nodo aca (para rmq)
 	p->size = 1 + size(p->l) + size(p->r);
 	p->mini = min(min(p->val, mini(p->l)), mini(p->r));//operacion del rmq!
-	p->parent=0;
-	if(p->l) p->l->parent=p;
-	if(p->r) p->r->parent=p;
+	p->parent = 0;
+	if(p->l) p->l->parent = p;
+	if(p->r) p->r->parent = p;
 }
 //junta dos arreglos
-pnode merge(pnode l, pnode r) {
-	if (!l || !r) return l ? l : r;
+pnode merge(pnode l, pnode r){
+	if(!l || !r) return l ? l : r;
 	push(l), push(r);
 	pnode t;
-	if (l->prior < r->prior) l->r=merge(l->r, r), t = l;
+	if(l->prior < r->prior) l->r=merge(l->r, r), t = l;
 	else r->l=merge(l, r->l), t = r; 
 	pull(t);
 	return t;
 }
 //parte el arreglo en dos, sz(l)==tam
-void split(pnode t, int tam, pnode &l, pnode &r) {
-	if (!t) return void(l = r = 0);
+void split(pnode t, int tam, pnode &l, pnode &r){
+	if(!t) return void(l = r = 0);
 	push(t);
-	if (tam <= size(t->l)) split(t->l, tam, l, t->l), r = t; 
+	if(tam <= size(t->l)) split(t->l, tam, l, t->l), r = t; 
 	else split(t->r, tam - 1 - size(t->l), t->r, r), l = t; 
 	pull(t);
 }
-pnode at(pnode t, int pos) {
+pnode at(pnode t, int pos){
 	if(!t) exit(1);
 	push(t);
 	if(pos == size(t->l)) return t;
 	if(pos < size(t->l)) return at(t->l, pos);
 	return at(t->r, pos - 1 - size(t->l));
 }
-int getpos(pnode t){//inversa de at
+int getpos(pnode t){ // inversa de at
 	if(!t->parent) return size(t->l);
-	if(t==t->parent->l) return getpos(t->parent)-size(t->r)-1;
-	return getpos(t->parent)+size(t->l)+1;
+	if(t == t->parent->l) return getpos(t->parent) - size(t->r) - 1;
+	return getpos(t->parent) + size(t->l) + 1;
 }
-void split(pnode t, int i, int j, pnode &l, pnode &m, pnode &r) {
-	split(t, i, l, t), split(t, j-i, m, r);}
-Value get(pnode &p, int i, int j){//like rmq
-	pnode l,m,r;
-    split(p, i, j, l, m, r);
-    Value ret=mini(m);
-    p=merge(l, merge(m, r));
-    return ret;
+void split(pnode t, int i, int j, pnode &l, pnode &m, pnode &r){
+	split(t, i, l, t), split(t, j-i, m, r);
 }
-void print(const pnode &t) {//for debugging
+Value get(pnode &p, int i, int j){ // like rmq
+	pnode l, m, r;
+  split(p, i, j, l, m, r);
+  Value ret = mini(m);
+  p = merge(l, merge(m, r));
+  return ret;
+}
+void print(const pnode &t){ // for debugging
 	if(!t) return;
-    push(t);
-    print(t->l);
-    cout << t->val.fst << ' ';
-    print(t->r);
+  push(t);
+  print(t->l);
+  cout << t->val.fst << ' ';
+  print(t->r);
 }
 
 //Sample program: C. LCA Online from Petrozavodsk Summer-2012. Petrozavodsk SU Contest
