d70e3b8caf182738ba1eafa8c5113885110e7627
Maxi Redigonda <maxiredigonda@gmail.com>
2017-10-08 12:25:32 -0300

    Arregla problemita con la contribución de Brian y modifica ldc

---
+++ math/combinatorio.cpp
@@ -0,0 +1,11 @@
+const int MAXC = 1000;
+ll C[MAXC+1][MAXC+1];
+forn(i, MAXC + 1){ //C[n][k] = C(n, k)
+    C[i][0] = C[i][i] = 1;
+    forr(k, 1, i) C[i][k] = (C[i-1][k] + C[i-1][k-1]) % MOD;
+}
+ll lucas(ll n, ll k, int p){ // Calcula C(n, k)%p teniendo C[p][p]
+    ll ans = 1;              // precalculado.
+    while(n + k){ ans = (ans * C[n%p][k%p]) % p; n/=p; k/=p; }
+    return ans;
+}
