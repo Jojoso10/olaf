43ca92ef41cde23f83c5a094388092728ff9d4eb
Maxi Redigonda <maxiredigonda@gmail.com>
2017-11-25 21:33:32 -0300

    Modifica el fenwick, por el getind

--- estructuras/fenwick.cpp
+++ estructuras/fenwick.cpp
@@ -1,7 +1,7 @@
 // Para 2D: tratar cada columna como un Fenwick Tree, 
 // agregando un for anidado en cada operacion.
 struct Fenwick {
-  static const int sz = 1000001;
+  static const int sz = 1 << 17 + 1;
   tipo t[sz];
   void adjust(int p, tipo v){ // p en [1, sz), O(lg n)
     for(int i = p; i < sz; i += (i & -i)) t[i] += v; 
@@ -12,16 +12,15 @@ struct Fenwick {
     return s;
   }
   tipo sum(int a, int b){ return sum(b) - sum(a - 1); }
-  // Obtener mayor valor con suma acumulada menor o igual que x.
-  // Para el menor, pasar x - 1 y sumar 1 al resultado.
-  int getind(tipo x){ // O(lg n)
-    int idx = 0, mask = n;
-    while(mask && idx < n) {
-      int z = idx + mask;
-      if(x >= t[z])
-        idx = z, x -= t[z];
-      mask >>= 1;
-    }
-    return idx;
+  // Obtiene el menor valor con suma acumulada >= x
+  // Para mayor, llamar a getind(v + 1) - 1, creo
+  int getind(tipo v) {
+    int x = 0;
+    for(int d = 1 << 17; d; d >>= 1)
+        if(t[x|d] < v){
+            x |= d;
+            v -= t[x];
+        }
+    return x + 1; 
   }
 };
